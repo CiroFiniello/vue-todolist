dc0ba053b27539bad84320dd3161e8076e780e14
CodeKiro <kirofiniello@outlook.it>
2024-06-03 02:07:12 +0200

    è stata creata la possibilità di aggiungere degli input di testo alla todolist

--- JS/script.js
+++ JS/script.js
@@ -1,14 +1,46 @@
 const { createApp } = Vue
 
 createApp({
-    data(){
-        return{
-            todoList : [
-
+    data() {
+        return {
+            todoList: [
+                { 
+                    content: 'Andare a Roma',
+                    done: false
+                },
+                {
+                    content: 'Mangiare la carbonara',
+                    done: true
+                },
+                { 
+                    content: 'Scavare le patate',
+                    done: false
+                },
+                { 
+                    content: 'Pulire i gatti',
+                    done: false
+                },
+                { 
+                    content: 'Buttare la spazzatura',
+                    done: false
+                },
             ],
+            newTaskContent: '',
         }
-},
-methods:{
-    
-},
+    },
+    methods: {
+        toggleDone(itemIndex) {
+            this.todoList[itemIndex].done = !this.todoList[itemIndex].done
+        },
+        addNewTask(newContent) {
+            const newTask = {
+                content: newContent,
+                done: false,
+            }
+            this.todoList.push(newTask);
+        },
+        clearNewTaskInput() {
+            this.newTaskContent = '';
+        }
+    }
 }).mount('#app');
